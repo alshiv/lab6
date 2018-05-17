16e06d8311c5c8e0a036a330677c5755dbc385ae
alshiv <fallos1997@gmail.com>
2018-05-17 13:44:06 +0700

    add function class

---
+++ minesweeper/minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace minesweeper
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapImage mine = new BitmapImage(new Uri(@"pack://application:,,,/img/x.jpg", UriKind.Absolute));
+        pole pl = new pole();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            pole.Children.Clear();
+
+            pl.init( int.Parse(tb2.Text), int.Parse(tb1.Text) );
+            pl.plant_mines(int.Parse(tb3.Text));
+
+            //указыается количество строк и столбцов в сетке
+            pole.Rows = int.Parse(tb1.Text);
+            pole.Columns = int.Parse(tb2.Text);
+            //указываются размеры сетки (число ячеек * (размер кнопки в ячейки + толщина её границ))
+            pole.Width = int.Parse(tb2.Text) * (50 + 4);
+            pole.Height = int.Parse(tb1.Text) * (50 + 4);
+            //толщина границ сетки
+            pole.Margin = new Thickness(3);
+
+            for (int i = 0; i < int.Parse(tb1.Text) * int.Parse(tb2.Text); i++)
+            {
+                //создание кнопки
+                Button btn = new Button();
+                //запись номера кнопки
+                btn.Tag = i;
+                //установка размеров кнопки
+                btn.Width = 50;
+                btn.Height = 50;
+                //текст на кнопке
+                btn.Content = " ";
+                //толщина границ кнопки
+                btn.Margin = new Thickness(2);
+                //при нажатии кнопки, будет вызываться метод Btn_Click
+                btn.Click += Btn_Click; ;
+                //добавление кнопки в сетку
+                pole.Children.Add(btn);
+            }
+
+            this.Width = 6 * 70;
+            this.Height = 7 * 70;
+
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            ////получение значения лежащего в Tag
+            int n = (int)((Button)sender).Tag;
+            ////установка фона нажатой кнопки, цвета и размера шрифта
+            //((Button)sender).Background = Brushes.White;
+            //((Button)sender).Foreground = Brushes.Red;
+            //((Button)sender).FontSize = 23;
+            ////запись в нажатую кнопку её номера
+            //((Button)sender).Content = (n%5).ToString() + " " + (n/5).ToString();
+
+
+
+
+            if (pl.getCell(n % int.Parse(tb2.Text) , n / int.Parse(tb1.Text)) == 9)
+            {
+                //создание и инициализация переменной для хранения изображения мины
+                Image img = new Image();
+                //загрузка изображения mine.jpg из папки imgs
+                img.Source = mine;
+
+                //создание глобальной переменной для отображения изображения мины
+                StackPanel minePnl;
+
+                //инициализация и установка ориентации, можно вызвать в методе инициализации формы
+                minePnl = new StackPanel();
+                // minePnl.Orientation = Orientation.Horizontal;
+                //установка толщины границы объекта
+                minePnl.Margin = new Thickness(1);
+                //добавление в объект изображения
+                minePnl.Children.Add(img);
+                ((Button)sender).Content = minePnl;
+            }
+            else
+            {
+                ////установка фона нажатой кнопки, цвета и размера шрифта
+                ((Button)sender).Background = Brushes.White;
+                ((Button)sender).Foreground = Brushes.Red;
+                ((Button)sender).FontSize = 23;
+                ///запись в нажатую кнопку её номера
+                ((Button)sender).Content = pl.getCell(n % int.Parse(tb2.Text), n / int.Parse(tb1.Text));
+            }
+        }
+    }
+}
---
+++ minesweeper/minesweeper/pole.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper
+{
+    class pole
+    {
+        int[,] field;
+
+        //создание массива m на n
+        public void init(int i, int j)
+        {
+            field = new int[i, j];
+        }
+
+        bool isBroken (int x, int y)
+        {
+            bool res = true;
+
+            int xmin = x - 1;
+            int ymin = y - 1;
+
+            if (xmin < 0) xmin = 0;
+            if (ymin < 0) ymin = 0;
+
+            int xmax = x + 1;
+            int ymax = y + 1;
+
+            if (xmax > field.GetLength(0)-1) xmax = field.GetLength(0)-1;
+            if (ymax > field.GetLength(1)-1) ymax = field.GetLength(1)-1;
+
+            for (int i = xmin; i <= xmax; i++)
+            {
+                for (int j = ymin; j <= ymax; j++)
+                {
+                    if (field[i, j] == 0)
+                    {
+                        res = false;
+                        break;
+                    }
+
+                }
+                if (res == false)
+                    break;
+            }
+                    return res;
+        }
+
+        public void plant_mines(int n)
+        {
+            Random rng = new Random();
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = rng.Next(field.GetLength(0));
+                int y = rng.Next(field.GetLength(1));
+
+                if (field[x, y] == 9)
+                {
+                    i--;
+                    continue;
+                }
+
+                field[x, y] = 9;
+
+                for (int i1 = 0; i1 < field.GetLength(0); i1++)
+                {
+                    for (int j1 = 0; j1 < field.GetLength(1); j1++)
+                        if (field[i1, j1] == 9)
+                            if (isBroken(i1, j1) == true)
+                            {
+                                i--;
+                                field[x, y] = 0;
+                                break;
+                            }
+                    if (field[x, y] == 0)
+                        break;
+                }
+            }
+
+        }
+
+        public void calculate()
+        {
+            //смотрим соседние клетки, считаем мины, сумму пишем в текущую
+        }
+
+        public int getCell(int i, int j)
+        {
+            return field[i, j];
+        }
+
+
+    }
+}
